8c7524d8787d67045b188dec6157fc06027d8e69
rajatmehta25 <rjt25881@gmail.com>
2021-11-01 22:33:37 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main'
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter,HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
   <>
-  <BrowserRouter>
+  <HashRouter>
   <Main />
-  </BrowserRouter>
+  </HashRouter>
 
   </>,
   document.getElementById('root')
