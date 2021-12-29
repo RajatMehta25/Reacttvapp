c4dd0997923a194880fd65109922bdf83ac2ec0a
RajatMehta <rajat.mehta@codeaegis.in>
2021-12-29 20:07:17 +0530

    update

--- src/Card.jsx
+++ src/Card.jsx
@@ -21,7 +21,7 @@ const Card = (props) => {
   <div className="card-body text-center">
     <h5 className="card-title" name="title">{props.title}</h5>
     {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-    <div ><a href={props.link} target="_blank" rel="noreferrer" className="btn btn-primary">{props.watch}</a></div>
+    <div ><a href={props.link} target="_parent" rel="noreferrer" className="btn btn-primary">{props.watch}</a></div>
   </div>
               </div>
         </>
