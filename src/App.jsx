af35c6cc6db14ff903fb9631033edbc6eb0a59e9
RajatMehta <rajat.mehta@codeaegis.in>
2021-12-29 14:30:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ key={index}
 }}
 
 
-function zcard(val){
+function zcard(val,i){
   console.log(val);
   if (val!==null){
     const lower=(val.title===null?"":val.title);
@@ -151,7 +151,9 @@ function zcard(val){
 if(x){
 
     return (
-        <Card title={val.title} 
+        <Card 
+        key={i}
+        title={val.title} 
               link={val.link}
         imgsrc={val.imgsrc}
         watch={val.watch}
@@ -173,9 +175,9 @@ return (
     <div className="namee"><a className="namee__word" href="https://www.instagram.com/thatsuaveboy/">RAJAT MEHTA</a></div>
    </div>
     <Navbar/>
-    <div id="search1"><input type="text" name="search" placeholder="SEARCH , ऐथे लब्बो" id="search" value={search2} onChange={Inputevent}/><span className="material-icons"  onClick={Inputevent2} ><IconButton><Mic  /></IconButton></span></div>
+    <div id="search1"><input type="text" name="search" placeholder="SEARCH , ऐथे लब्बो" id="search" value={search2} onChange={Inputevent}/><span className="material-icons"  onClick={Inputevent2} ><IconButton><Mic style={{marginTop:15}}  /></IconButton></span></div>
     
-  <div className="rendered" style={sty}>{search2===''?<><div style={{textAlign:'center'}}><h1 style={{color:'white',fontFamily: 'Lato'}}>ENTER CHANNEL NAME <LockOpenIcon/> </h1><img style={{borderRadius:10,height:"60vh",width:"60vw"}} src="https://www.denofgeek.com/wp-content/uploads/2021/05/Tom-Cruise-in-Mission-Impossible-Vault.jpeg?resize=768%2C432" /></div></>:JSON.parse(allData).map(zcard)}</div>
+  <div className="rendered" style={sty}>{search2===''?<><div style={{textAlign:'center'}}><h1 style={{color:'white',fontFamily: 'Lato'}}>ENTER CHANNEL NAME <LockOpenIcon style={{marginTop:-10}}/> </h1><img style={{borderRadius:10,height:"59vh",width:"90vw"}} src="https://media4.giphy.com/media/Mb9dQnfZXSBYMhU2Nv/giphy.gif" /></div></>:JSON.parse(allData).map(zcard)}</div>
     </>
 
 
