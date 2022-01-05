f4eef8719571bd3a32c584aa037e6156b076e97b
RajatMehta <rajat.mehta@codeaegis.in>
2022-01-05 16:53:52 +0530

    update

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-
+import './Movies.css'
+import Navbar from './Navbar'
 const Movies = () => {
+    // const Dostuff=()=>{
+    //     var ifra=document.getElementById('iframe')
+    //     console.log(ifra.contentDocument);
+    // }
  
     return (
         <>
+         <div className="header">
+    <h4 className="heading">Developed By :</h4>
+    <div className="namee"><a className="namee__word" href="https://www.instagram.com/thatsuaveboy/">RAJAT MEHTA</a></div>
+   </div>
+    <Navbar/>
    <h1 style={{color:'white'}}> Movies coming soon...</h1>  
-   {/* <iframe style={{overflow:"hidden"}} scrolling="no" src="http://103.222.20.150/ftpdata/Movies/Bollywood/" title="Movies"> */}
-       
-   {/* </iframe> */}
+  {/* <iframe src="http://103.222.20.150/ftpdata/Movies/Bollywood/" referrerPolicy="" id="iframe" title="aa" onLoad={Dostuff()}></iframe> */}
         </>
     )
 }
