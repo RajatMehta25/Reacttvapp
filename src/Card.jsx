d6a210df16e29519e5353843a05a6cac13eaf0aa
RajatMehta <rajat.mehta@codeaegis.in>
2022-01-06 12:14:33 +0530

    update

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,23 +8,27 @@ import "../node_modules/animate.css";
 
 
 
-const styling={width:"250px",margin:"10px"}
+const styling={width:"180px",height:180,margin:"30px",alignItems:'center',justifyContent:'center',textAlign:'center'}
 const Card = (props) => {
 
     return (
         <>
         <div className="card" style={styling}>
-        <img src={props.imgsrc} className="card-img-top" alt="SABRR KRO"/>
+     
+        <a href={props.qua?props.link+props.qua:props.link} target="_parent" rel="noreferrer" className="" stle={{padding:30,textDecoration:'none',}}> <img src={props.imgsrc} style={{borderRadius:999999}} className="card-img-top" alt="SABRR KRO"  /></a>
         
+        <span style={{textDecoration:'none',color:'white',fontWeight:'bold',fontFamily:'Lato',position:'absolute',marginTop:220}}>{props.title}</span>
+
             
   
-  <div className="card-body text-center">
-    <h5 className="card-title" name="title">{props.title}</h5>
+  {/* <div className="card-body text-center"> */}
+    {/* <h5 className="card-title" name="title">{props.title}</h5> */}
     {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-    <div ><a href={props.qua?props.link+props.qua:props.link} target="_parent" rel="noreferrer" className="btn btn-primary">{props.watch}</a></div>
+    {/* <div ><a href={props.qua?props.link+props.qua:props.link} target="_parent" rel="noreferrer" className="btn btn-primary">{props.watch}</a></div> */}
     
-  </div>
+  {/* </div> */}
               </div>
+              
         </>
 
     );
